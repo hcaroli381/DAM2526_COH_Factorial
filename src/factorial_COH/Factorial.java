d94c3842ef019b736a847d09d2cd8365ea8fef6a
hcaroli381 <hcaroli381@g.educaand.es>
2025-12-10 08:37:55 +0100

    Modularizada la función de pedir un numero

--- src/factorial_COH/Factorial.java
+++ src/factorial_COH/Factorial.java
@@ -3,12 +3,11 @@ package factorial_COH;
 import java.util.Scanner;
 
 public class Factorial {
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Introduce un número entero mayor o igual que 0: ");
-		int num = sc.nextInt();
+		int num;
+		num = pedirNumero();
 
 		if (num < 0) {
 			System.out.println("ERROR: el número debe ser mayor o igual que 0.");
@@ -16,7 +15,11 @@ public class Factorial {
 			System.out.println("Has introducido el número: " + num);
 		}
 
-		sc.close();
+	}
+
+	private static int pedirNumero() {
+		System.out.print("Introduce un número entero mayor o igual que 0: ");
+		return sc.nextInt();
 	}
 
 }
